0e9c878878515e5a4885973c143e2b2a39ade5c9
Rolland Yann <rollandyann@gmail.com>
2017-11-07 19:10:40 +0100

    Header modify

--- header.php
+++ header.php
@@ -3,16 +3,19 @@
 <head>
 	
 	<meta charset="<?php bloginfo('charset'); ?>">
+
+	<meta http-equiv="x-ua-compatible" content="ie=edge">	
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
 	<?php
-		$fluxi_excerpt = get_field('extrait', false, false);
+		$fluxi_excerpt = get_field('extrait_liste', false, false);
 		if ($fluxi_excerpt) {
 			echo '<meta name="description" content="'.$fluxi_excerpt.'">';
 		} else {
 			echo '<meta name="description" content="'.get_bloginfo('description').'">';
 		}
 	?>
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
 	<title><?php get_template_part( 'page-templates-parts/base/title'); ?></title>
 	
