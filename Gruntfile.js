460162024db5df0d2d5bb37f8c4c6b8c39f48033
Thibaut Caroli <thibaut.caroli.pro@gmail.com>
2023-11-16 10:37:06 +0100

    Fix Grunt & export ACF

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function (grunt) {
 		    options: {
 		        map: false,
 		        processors: [
-		          	require('autoprefixer')({browsers: ['last 2 versions']}),
+		          	require('autoprefixer')({overrideBrowserslist: ['last 2 versions']}),
 		          	require('cssnano')()
 		        ]
 			},
@@ -86,7 +86,7 @@ module.exports = function (grunt) {
 	        }
 		},
 
-		
+
 
 	});
 
